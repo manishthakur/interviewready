ce0829ff69bbb7f553a71e3c87b8b9bc5d4c3ca1
Manish Thakur <neelamthakur.in@gmail.com>
2022-11-15 23:01:49 -0500

    Initial commit

---
+++ src/main/java/com/mt/interviewready/InterviewreadyApplication.java
@@ -0,0 +1,13 @@
+package com.mt.interviewready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewreadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewreadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mt/interviewready/InterviewreadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.interviewready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewreadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
